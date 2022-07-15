0bf9d2ab0c741c49843277eab78aa10111cc7eca
Dov Davidovics <dov@medadoc.com>
2022-07-15 13:57:35 -0700

    working button to toggle color back and forth

--- computeAge/script.js
+++ computeAge/script.js
@@ -1,6 +1,13 @@
 console.log("at start of script.js in computeAge app");
+let state = 1;
 function handleClickChange() {
   console.log("click change button");
   const floatContainer = document.getElementById("float-container");
-  floatContainer.setAttribute("class", "float-container-alt");
+  if (state == 1) {
+    state = 0;
+    floatContainer.setAttribute("class", "float-container");
+  } else {
+    floatContainer.setAttribute("class", "float-container-alt");
+    state = 1;
+  }
 }
