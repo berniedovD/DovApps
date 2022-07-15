1d8954c8ee1aeb20df880dfde0c5ab3dd70dd5ac
Dov Davidovics <dov@medadoc.com>
2022-07-15 11:36:56 -0700

    add computeAge app

---
+++ computeAge/script.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello World</h1>");
+document.write('<label for="fname">First name:</label>');
+document.write('<input type="text" id="fname" name="fname"><br><br>');
+for (let i = 0; i < 10; i++) {
+  document.write(`<h1 class='red'>  ${i} </h1>`);
+  console.log(i);
+}
+console.log(fname);
