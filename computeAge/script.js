9732df2dfd0930bbb49b13c8406c8520c5374ea8
Dov Davidovics <dov@medadoc.com>
2022-07-18 14:27:38 -0700

    add reset button and format buttons

--- computeAge/script.js
+++ computeAge/script.js
@@ -1,5 +1,9 @@
 import { displayAge } from "./displayAge.js";
 import { fmtDate } from "./fmtDate.js";
+function reloadPage() {
+  console.log("in reloadPage");
+  location.reload();
+}
 console.log("at top of script.js");
 const today = new Date();
 const { fmtDate: fmtToday } = fmtDate(today);
@@ -7,3 +11,5 @@ let showToday = document.getElementById("todayd");
 showToday.textContent = fmtToday;
 let subBtn = document.getElementById("subBtn");
 subBtn.addEventListener("click", displayAge);
+let resetBtn = document.getElementById("reload");
+resetBtn.addEventListener("click", reloadPage);
