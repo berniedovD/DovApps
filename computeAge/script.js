4826fd585b97d64b3623a3e9212e5211b3e6a834
Dov Davidovics <dov@medadoc.com>
2022-07-18 11:34:10 -0700

    playing around

--- computeAge/script.js
+++ computeAge/script.js
@@ -1,26 +1,31 @@
-import { computeAge } from ".computeAge.js";
-const today = new Date();
-console.log(today);
-const yyyy = today.getFullYear();
-const mm = today.getMonth() + 1;
-const dd = today.getDate();
-const fmtToday = `Today's Date=${mm}/${dd}/${yyyy}`;
-showToday = document.getElementById("todayd");
-console.log(showToday);
-showToday.textContent = fmtToday;
+import { computeAge } from "./computeAge.js";
 
-function displayAge() {
-  console.log("inside the computeAge function");
-  const inDate = document.getElementById("DOB").value;
-  year = parseInt(inDate.substring(0, 4));
-  month = parseInt(inDate.substring(5, 7));
-  day = parseInt(inDate.substring(8, 10));
-  const today = new Date();
+function fmtDate(dateOBJ) {
+  console.log("inside fmtDate");
   console.log(today);
   const yyyy = today.getFullYear();
   const mm = today.getMonth() + 1;
   const dd = today.getDate();
+  const fmtToday = `Today's Date=${mm}/${dd}/${yyyy}`;
+  const retDate = { fmtDate: fmtToday, mm: mm, dd: dd, yyyy: yyyy };
+  return retDate;
+}
 
+function displayAge() {
+  console.log("inside the displayAge function");
+  console.log("document ...");
+  console.log(document);
+  const inDate = document.getElementById("DOB").value;
+  console.log("inDate ...");
+  console.log(inDate);
+  let DOB = fmtDate(inDate);
+  let year = DOB.yyyy;
+
+  let month = DOB.mm;
+  let day = DOB.dd;
+  const today = new Date();
+  const { fmtDate: fmtToday } = fmtDate(today);
+  console.log(`year=${year}`);
   let dAge = document.getElementById("displayAge");
   let age = computeAge(year, month, day, yyyy, mm, dd);
   const { ageYears, ageMonths } = age;
@@ -30,3 +35,11 @@ function displayAge() {
     dAge.textContent = "could not compute age - enter proper DOB";
   }
 }
+const today = new Date();
+const { fmtDate: fmtToday } = fmtDate(today);
+
+let showToday = document.getElementById("todayd");
+console.log(showToday);
+showToday.textContent = fmtToday;
+let subBtn = document.getElementById("subBtn");
+subBtn.addEventListener("click", displayAge());
