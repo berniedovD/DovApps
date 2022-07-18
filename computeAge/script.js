4715daecf509247380d3835bb7ab80fe47122251
Dov Davidovics <dov@medadoc.com>
2022-07-18 14:33:07 -0700

    reload page with arrow function

--- computeAge/script.js
+++ computeAge/script.js
@@ -1,9 +1,6 @@
 import { displayAge } from "./displayAge.js";
 import { fmtDate } from "./fmtDate.js";
-function reloadPage() {
-  console.log("in reloadPage");
-  location.reload();
-}
+
 console.log("at top of script.js");
 const today = new Date();
 const { fmtDate: fmtToday } = fmtDate(today);
@@ -12,4 +9,4 @@ showToday.textContent = fmtToday;
 let subBtn = document.getElementById("subBtn");
 subBtn.addEventListener("click", displayAge);
 let resetBtn = document.getElementById("reload");
-resetBtn.addEventListener("click", reloadPage);
+resetBtn.addEventListener("click", () => location.reload());
