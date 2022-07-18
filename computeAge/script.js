751538d6a6f40edec7d133e77760a0ebf1e0f818
Dov Davidovics <dov@medadoc.com>
2022-07-18 14:42:46 -0700

    playing

--- computeAge/script.js
+++ computeAge/script.js
@@ -2,10 +2,9 @@ import { displayAge } from "./displayAge.js";
 import { fmtDate } from "./fmtDate.js";
 
 console.log("at top of script.js");
-const today = new Date();
-const { fmtDate: fmtToday } = fmtDate(today);
-let showToday = document.getElementById("todayd");
-showToday.textContent = fmtToday;
+const { fmtDate: fmtToday } = fmtDate(new Date());
+document.getElementById("todayd").textContent = fmtToday;
+
 let subBtn = document.getElementById("subBtn");
 subBtn.addEventListener("click", displayAge);
 let resetBtn = document.getElementById("reload");
