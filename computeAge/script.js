c2672b072a53920e51bd0dd2bc46f65aca88f3b9
Dov Davidovics <dov@medadoc.com>
2022-07-18 14:54:46 -0700

    working - after refactor calls to setup document buttons

--- computeAge/script.js
+++ computeAge/script.js
@@ -2,6 +2,7 @@ import { displayAge } from "./displayAge.js";
 import { fmtDate } from "./fmtDate.js";
 
 console.log("at top of script.js");
+
 const { fmtDate: outDate } = fmtDate(new Date());
 document.getElementById("todayd").textContent = outDate;
 
