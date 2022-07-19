6ffef4f1f6579e39863ace65f498765f250b09fc
Dov Davidovics <dov@medadoc.com>
2022-07-19 09:26:29 -0700

    add java hello world

---
+++ java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println("Hello World ----");
+      String age = computeAge();
+      System.out.println(age);
+
+    }
+    static String computeAge() {
+        System.out.println("in computeAge");
+        String ageString = "50:10:10";
+        return ageString;
+    }
+  }
+
+
+  
