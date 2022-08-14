e8606e46537a532bd4ade561d244d76a862a5c0c
Dov Davidovics <dov@medadoc.com>
2022-08-14 14:26:00 -0700

    jewish calendar python start

---
+++ jCalPython/fourgates.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  7 22:35:16 2022
+
+@author: medadocadmin
+"""
+
+fgates = [
+    {'rowNum': 1, 'moladLimit': '7:18:0', 'yearCycle': [
+        {'yearNum': 1, 'keviyah': '2D3'},
+        {'yearNum': 2, 'keviyah': '5R7'},
+        {'yearNum': 3, 'keviyah': '5R7'},
+        {'yearNum': 4, 'keviyah': '2D3'},
+        {'yearNum': 5, 'keviyah': '5R7'},
+        {'yearNum': 6, 'keviyah': '5R7'},
+        {'yearNum': 7, 'keviyah': '2D3'},
+        {'yearNum': 8, 'keviyah': '5R7'},
+        {'yearNum': 9, 'keviyah': '2D3'},
+        {'yearNum': 10, 'keviyah': '5R7'},
+        {'yearNum': 11, 'keviyah': '5R7'},
+        {'yearNum': 12, 'keviyah': '2D3'},
+        {'yearNum': 13, 'keviyah': '5R7'},
+        {'yearNum': 14, 'keviyah': '5R7'},
+        {'yearNum': 15, 'keviyah': '2D3'},
+        {'yearNum': 16, 'keviyah': '5R7'},
+        {'yearNum': 17, 'keviyah': '5R7'},
+        {'yearNum': 18, 'keviyah': '2D3'},
+        {'yearNum': 19, 'keviyah': '5R7'}
+
+        ]}
+    
+    ]
+
